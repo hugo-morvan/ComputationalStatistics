952cbd7c091d235c42682429a9e39d2143e52b1f
dbozz26 <144434851+dbozz26@users.noreply.github.com>
2023-12-05 22:11:11 +0100

    update lab5

--- dani workspace.R
+++ dani workspace.R
@@ -6,7 +6,7 @@ rm(list=ls(all.names=T))
 
 data <- read.csv("prices1.csv", sep=";")
 
-### 1)
+## 1)
 
 price <- data$Price
 sqft <- data$SqFt
@@ -15,9 +15,9 @@ plot(sqft, price)
 fit1 <- lm(Price ~ SqFt, data)
 lines(sqft, fit1$fitted.values, col="darkred", type="l", lwd=1)
 
-# answer a:
+# A straight line seems to summarize the data pretty well.
 
-### 2)
+## 2)
 
 fit2 <- function(c, data){
   data$new <- rep(0,110)
@@ -29,9 +29,71 @@ fit2 <- function(c, data){
   sum(lm2$residuals^2)
 }
 
-RSS_opt <- optim(2000, fit2, data=data, method="Brent", lower=0, upper=4500)
+RSS_opt <- optim(2000, fit2, data=data, method="L-BFGS-B", lower=0, upper=4500)
+RSS_opt$par
+RSS_opt$value
 
-### 3)
+## 3)
 
+library(boot)
 
+c_val <- c()
 
+for (i in 1:2E3){
+  if (i%%500==0) cat(i, "\r")
+  id <- sample(1:110, 110, replace = TRUE)
+  data_boot <- data[id,]
+  c_opt <- optim(2000, fit2, data=data_boot, method="L-BFGS-B", lower=0, upper=4500)   
+  c_val <<- c(c_val, c_opt$par)
+}
+
+hist(c_val)
+
+2 * RSS_opt$par - sum(c_val) / 2000      # bootstrap bias-correction
+sum((c_val - mean(c_val))^2) / (2000-1)  # variance of c
+
+opt_boot <- function(data, id){
+  id <- sample(1:110, 110, replace = TRUE)
+  data_boot2 <- data[id,]
+  c_opt2 <- optim(2000, fit2, data= data_boot2, method = "L-BFGS-B", lower = 0, upper = 4500)   
+  c_opt2$par
+}
+
+boot_fun <- boot(data = data, opt_boot, R = 2E3)
+boot.ci(boot_fun)
+plot(boot_fun)
+
+# We seem to obtain two Gaussian-ish shaped distributions.
+
+## 4)
+
+jack_c <-c()
+
+# new function for the jackknife function with 109 rows instead of 110
+fit3 <- function(c, data){
+  data$new <- rep(0,109)
+  for (i in 1:nrow(data)){
+    if (data[i,2] > c) data[i,5] <- data[i,2] - c
+    else data[i,5] <- 0
+  }
+  lm2 <- lm(Price ~ SqFt + new, data)
+  sum(lm2$residuals^2)
+}
+
+for (i in 1:110){
+  id <- sample(1:110, 110, replace = TRUE)
+  data_boot3 <- data[id,]
+  data_jack <- data_boot3[-i,]
+  c_opt <- optim(2000, fit3, data=data_jack, method = "L-BFGS-B", lower = 0, upper = 4500)   
+  jack_c <<- c(jack_c, c_opt$par)
+}
+
+jack_c
+var(jack_c)
+
+# (The values we obtain are not accurate, we will wait for feedback to
+# understand what we did wrong.)
+
+## 5) 
+
+#
